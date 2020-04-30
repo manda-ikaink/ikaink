8ac4eb36bd0262b9a8f7fe6e8a3a1b5731a7114a
Amanda Eldreth <manda.eldreth@gmail.com>
2020-04-29 21:02:09 -0700

    Add disqus commenting

--- views/matrix/scrapbook/post.blade.php
+++ views/matrix/scrapbook/post.blade.php
@@ -19,6 +19,7 @@
                 <a href="{{ url($postcat->path()) }}">{{ $postcat->name }}</a>@unless($loop->last), @endunless
             @endforeach
             @endif
+            {{-- <a href="{{ url($entry->uri) }}#disqus_thread">Post a Comment</a> --}}
         </p>
     </div>
     
@@ -42,9 +43,30 @@
         @includeIf('components.page.' . $sectionSet['handle'], ['component' => $section['data']])
     @endforeach
     @endunless
+
+    <section class="container mb-5">
+        <hr>
+        <h3 class="text-display--xxs"><a class="text-display--default" href="{{ url($entry->uri) }}#disqus_thread">Comment</a></h3>
+        <div id="disqus_thread"></div>
+    </section>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 </div>
 @endsection
 
 @push('bottomScripts')
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-512873b2612d88df"></script>
+{{-- <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-512873b2612d88df"></script> --}}
+<script>
+    var disqus_config = function () {
+        this.page.url = '{{ url($entry->uri) }}';  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = 'scrapbook-post-{{ $entry->id }}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        this.page.title = '{{ $entry->display_name ? $entry->display_name : $entry->name }}';
+    };
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://ika-ink.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+    </script>
+    <script id="dsq-count-scr" src="//ika-ink.disqus.com/count.js" async></script>
 @endpush
